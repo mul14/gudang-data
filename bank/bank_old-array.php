c21b179f740cd3ff8112de2ef7bbe201e96b5986
Mulia Arifandy Nasution <mul14.net@gmail.com>
2014-06-05 11:53:56 +0700

    Tambahin versi PHP

---
+++ bank/bank_old-array.php
@@ -0,0 +1,543 @@
+<?php
+$bank = array(
+    array(
+        "name" => "BANK BRI",
+        "code" => "002"
+    ),
+    array(
+        "name" => "BANK EKSPOR INDONESIA",
+        "code" => "003"
+    ),
+    array(
+        "name" => "BANK MANDIRI",
+        "code" => "008"
+    ),
+    array(
+        "name" => "BANK BNI",
+        "code" => "009"
+    ),
+    array(
+        "name" => "BANK DANAMON",
+        "code" => "011"
+    ),
+    array(
+        "name" => "PERMATA BANK",
+        "code" => "013"
+    ),
+    array(
+        "name" => "BANK BCA",
+        "code" => "014"
+    ),
+    array(
+        "name" => "BANK BII",
+        "code" => "016"
+    ),
+    array(
+        "name" => "BANK PANIN",
+        "code" => "019"
+    ),
+    array(
+        "name" => "BANK ARTA NIAGA KENCANA",
+        "code" => "020"
+    ),
+    array(
+        "name" => "BANK NIAGA",
+        "code" => "022"
+    ),
+    array(
+        "name" => "BANK BUANA IND",
+        "code" => "023"
+    ),
+    array(
+        "name" => "BANK LIPPO",
+        "code" => "026"
+    ),
+    array(
+        "name" => "BANK NISP",
+        "code" => "028"
+    ),
+    array(
+        "name" => "AMERICAN EXPRESS BANK LTD",
+        "code" => "030"
+    ),
+    array(
+        "name" => "CITIBANK N.A.",
+        "code" => "031"
+    ),
+    array(
+        "name" => "JP. MORGAN CHASE BANK, N.A.",
+        "code" => "032"
+    ),
+    array(
+        "name" => "BANK OF AMERICA, N.A",
+        "code" => "033"
+    ),
+    array(
+        "name" => "ING INDONESIA BANK",
+        "code" => "034"
+    ),
+    array(
+        "name" => "BANK MULTICOR TBK.",
+        "code" => "036"
+    ),
+    array(
+        "name" => "BANK ARTHA GRAHA",
+        "code" => "037"
+    ),
+    array(
+        "name" => "BANK CREDIT AGRICOLE INDOSUEZ",
+        "code" => "039"
+    ),
+    array(
+        "name" => "THE BANGKOK BANK COMP. LTD",
+        "code" => "040"
+    ),
+    array(
+        "name" => "THE HONGKONG & SHANGHAI B.C.",
+        "code" => "041"
+    ),
+    array(
+        "name" => "THE BANK OF TOKYO MITSUBISHI UFJ LTD",
+        "code" => "042"
+    ),
+    array(
+        "name" => "BANK SUMITOMO MITSUI INDONESIA",
+        "code" => "045"
+    ),
+    array(
+        "name" => "BANK DBS INDONESIA",
+        "code" => "046"
+    ),
+    array(
+        "name" => "BANK RESONA PERDANIA",
+        "code" => "047"
+    ),
+    array(
+        "name" => "BANK MIZUHO INDONESIA",
+        "code" => "048"
+    ),
+    array(
+        "name" => "STANDARD CHARTERED BANK",
+        "code" => "050"
+    ),
+    array(
+        "name" => "BANK ABN AMRO",
+        "code" => "052"
+    ),
+    array(
+        "name" => "BANK KEPPEL TATLEE BUANA",
+        "code" => "053"
+    ),
+    array(
+        "name" => "BANK CAPITAL INDONESIA, TBK.",
+        "code" => "054"
+    ),
+    array(
+        "name" => "BANK BNP PARIBAS INDONESIA",
+        "code" => "057"
+    ),
+    array(
+        "name" => "BANK UOB INDONESIA",
+        "code" => "058"
+    ),
+    array(
+        "name" => "KOREA EXCHANGE BANK DANAMON",
+        "code" => "059"
+    ),
+    array(
+        "name" => "RABOBANK INTERNASIONAL INDONESIA",
+        "code" => "060"
+    ),
+    array(
+        "name" => "ANZ PANIN BANK",
+        "code" => "061"
+    ),
+    array(
+        "name" => "DEUTSCHE BANK AG.",
+        "code" => "067"
+    ),
+    array(
+        "name" => "BANK WOORI INDONESIA",
+        "code" => "068"
+    ),
+    array(
+        "name" => "BANK OF CHINA LIMITED",
+        "code" => "069"
+    ),
+    array(
+        "name" => "BANK BUMI ARTA",
+        "code" => "076"
+    ),
+    array(
+        "name" => "BANK EKONOMI",
+        "code" => "087"
+    ),
+    array(
+        "name" => "BANK ANTARDAERAH",
+        "code" => "088"
+    ),
+    array(
+        "name" => "BANK HAGA",
+        "code" => "089"
+    ),
+    array(
+        "name" => "BANK IFI",
+        "code" => "093"
+    ),
+    array(
+        "name" => "BANK CENTURY, TBK.",
+        "code" => "095"
+    ),
+    array(
+        "name" => "BANK MAYAPADA",
+        "code" => "097"
+    ),
+    array(
+        "name" => "BANK JABAR",
+        "code" => "110"
+    ),
+    array(
+        "name" => "BANK DKI",
+        "code" => "111"
+    ),
+    array(
+        "name" => "BPD DIY",
+        "code" => "112"
+    ),
+    array(
+        "name" => "BANK JATENG",
+        "code" => "113"
+    ),
+    array(
+        "name" => "BANK JATIM",
+        "code" => "114"
+    ),
+    array(
+        "name" => "BPD JAMBI",
+        "code" => "115"
+    ),
+    array(
+        "name" => "BPD ACEH",
+        "code" => "116"
+    ),
+    array(
+        "name" => "BANK SUMUT",
+        "code" => "117"
+    ),
+    array(
+        "name" => "BANK NAGARI",
+        "code" => "118"
+    ),
+    array(
+        "name" => "BANK RIAU",
+        "code" => "119"
+    ),
+    array(
+        "name" => "BANK SUMSEL",
+        "code" => "120"
+    ),
+    array(
+        "name" => "BANK LAMPUNG",
+        "code" => "121"
+    ),
+    array(
+        "name" => "BPD KALSEL",
+        "code" => "122"
+    ),
+    array(
+        "name" => "BPD KALIMANTAN BARAT",
+        "code" => "123"
+    ),
+    array(
+        "name" => "BPD KALTIM",
+        "code" => "124"
+    ),
+    array(
+        "name" => "BPD KALTENG",
+        "code" => "125"
+    ),
+    array(
+        "name" => "BPD SULSEL",
+        "code" => "126"
+    ),
+    array(
+        "name" => "BANK SULUT",
+        "code" => "127"
+    ),
+    array(
+        "name" => "BPD NTB",
+        "code" => "128"
+    ),
+    array(
+        "name" => "BPD BALI",
+        "code" => "129"
+    ),
+    array(
+        "name" => "BANK NTT",
+        "code" => "130"
+    ),
+    array(
+        "name" => "BANK MALUKU",
+        "code" => "131"
+    ),
+    array(
+        "name" => "BPD PAPUA",
+        "code" => "132"
+    ),
+    array(
+        "name" => "BANK BENGKULU",
+        "code" => "133"
+    ),
+    array(
+        "name" => "BPD SULAWESI TENGAH",
+        "code" => "134"
+    ),
+    array(
+        "name" => "BANK SULTRA",
+        "code" => "135"
+    ),
+    array(
+        "name" => "BANK NUSANTARA PARAHYANGAN",
+        "code" => "145"
+    ),
+    array(
+        "name" => "BANK SWADESI",
+        "code" => "146"
+    ),
+    array(
+        "name" => "BANK MUAMALAT",
+        "code" => "147"
+    ),
+    array(
+        "name" => "BANK MESTIKA",
+        "code" => "151"
+    ),
+    array(
+        "name" => "BANK METRO EXPRESS",
+        "code" => "152"
+    ),
+    array(
+        "name" => "BANK SHINTA INDONESIA",
+        "code" => "153"
+    ),
+    array(
+        "name" => "BANK MASPION",
+        "code" => "157"
+    ),
+    array(
+        "name" => "BANK HAGAKITA",
+        "code" => "159"
+    ),
+    array(
+        "name" => "BANK GANESHA",
+        "code" => "161"
+    ),
+    array(
+        "name" => "BANK WINDU KENTJANA",
+        "code" => "162"
+    ),
+    array(
+        "name" => "HALIM INDONESIA BANK",
+        "code" => "164"
+    ),
+    array(
+        "name" => "BANK HARMONI INTERNATIONAL",
+        "code" => "166"
+    ),
+    array(
+        "name" => "BANK KESAWAN",
+        "code" => "167"
+    ),
+    array(
+        "name" => "BANK TABUNGAN NEGARA (PERSERO)",
+        "code" => "200"
+    ),
+    array(
+        "name" => "BANK HIMPUNAN SAUDARA 1906, TBK .",
+        "code" => "212"
+    ),
+    array(
+        "name" => "BANK TABUNGAN PENSIUNAN NASIONAL",
+        "code" => "213"
+    ),
+    array(
+        "name" => "BANK SWAGUNA",
+        "code" => "405"
+    ),
+    array(
+        "name" => "BANK JASA ARTA",
+        "code" => "422"
+    ),
+    array(
+        "name" => "BANK MEGA",
+        "code" => "426"
+    ),
+    array(
+        "name" => "BANK JASA JAKARTA",
+        "code" => "427"
+    ),
+    array(
+        "name" => "BANK BUKOPIN",
+        "code" => "441"
+    ),
+    array(
+        "name" => "BANK SYARIAH MANDIRI",
+        "code" => "451"
+    ),
+    array(
+        "name" => "BANK BISNIS INTERNASIONAL",
+        "code" => "459"
+    ),
+    array(
+        "name" => "BANK SRI PARTHA",
+        "code" => "466"
+    ),
+    array(
+        "name" => "BANK JASA JAKARTA",
+        "code" => "472"
+    ),
+    array(
+        "name" => "BANK BINTANG MANUNGGAL",
+        "code" => "484"
+    ),
+    array(
+        "name" => "BANK BUMIPUTERA",
+        "code" => "485"
+    ),
+    array(
+        "name" => "BANK YUDHA BHAKTI",
+        "code" => "490"
+    ),
+    array(
+        "name" => "BANK MITRANIAGA",
+        "code" => "491"
+    ),
+    array(
+        "name" => "BANK AGRO NIAGA",
+        "code" => "494"
+    ),
+    array(
+        "name" => "BANK INDOMONEX",
+        "code" => "498"
+    ),
+    array(
+        "name" => "BANK ROYAL INDONESIA",
+        "code" => "501"
+    ),
+    array(
+        "name" => "BANK ALFINDO",
+        "code" => "503"
+    ),
+    array(
+        "name" => "BANK SYARIAH MEGA",
+        "code" => "506"
+    ),
+    array(
+        "name" => "BANK INA PERDANA",
+        "code" => "513"
+    ),
+    array(
+        "name" => "BANK HARFA",
+        "code" => "517"
+    ),
+    array(
+        "name" => "PRIMA MASTER BANK",
+        "code" => "520"
+    ),
+    array(
+        "name" => "BANK PERSYARIKATAN INDONESIA",
+        "code" => "521"
+    ),
+    array(
+        "name" => "BANK AKITA",
+        "code" => "525"
+    ),
+    array(
+        "name" => "LIMAN INTERNATIONAL BANK",
+        "code" => "526"
+    ),
+    array(
+        "name" => "ANGLOMAS INTERNASIONAL BANK",
+        "code" => "531"
+    ),
+    array(
+        "name" => "BANK DIPO INTERNATIONAL",
+        "code" => "523"
+    ),
+    array(
+        "name" => "BANK KESEJAHTERAAN EKONOMI",
+        "code" => "535"
+    ),
+    array(
+        "name" => "BANK UIB",
+        "code" => "536"
+    ),
+    array(
+        "name" => "BANK ARTOS IND",
+        "code" => "542"
+    ),
+    array(
+        "name" => "BANK PURBA DANARTA",
+        "code" => "547"
+    ),
+    array(
+        "name" => "BANK MULTI ARTA SENTOSA",
+        "code" => "548"
+    ),
+    array(
+        "name" => "BANK MAYORA",
+        "code" => "553"
+    ),
+    array(
+        "name" => "BANK INDEX SELINDO",
+        "code" => "555"
+    ),
+    array(
+        "name" => "BANK VICTORIA INTERNATIONAL",
+        "code" => "566"
+    ),
+    array(
+        "name" => "BANK EKSEKUTIF",
+        "code" => "558"
+    ),
+    array(
+        "name" => "CENTRATAMA NASIONAL BANK",
+        "code" => "559"
+    ),
+    array(
+        "name" => "BANK FAMA INTERNASIONAL",
+        "code" => "562"
+    ),
+    array(
+        "name" => "BANK SINAR HARAPAN BALI",
+        "code" => "564"
+    ),
+    array(
+        "name" => "BANK HARDA",
+        "code" => "567"
+    ),
+    array(
+        "name" => "BANK FINCONESIA",
+        "code" => "945"
+    ),
+    array(
+        "name" => "BANK MERINCORP",
+        "code" => "946"
+    ),
+    array(
+        "name" => "BANK MAYBANK INDOCORP",
+        "code" => "947"
+    ),
+    array(
+        "name" => "BANK OCBC – INDONESIA",
+        "code" => "948"
+    ),
+    array(
+        "name" => "BANK CHINA TRUST INDONESIA",
+        "code" => "949"
+    ),
+    array(
+        "name" => "BANK COMMONWEALTH",
+        "code" => "950"
+    )
+);
